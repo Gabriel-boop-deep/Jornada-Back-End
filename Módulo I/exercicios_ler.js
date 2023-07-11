072751b7fca5877133d27f77f364927a5c01d425
Gabriel Nunes Barbosa Nogueira <nunesnbnxn@gmail.com>
2023-07-11 15:04:42 -0300

    Atualizando diretorio

--- Módulo I/exercicios_ler.js
+++ Módulo I/exercicios_ler.js
@@ -1,2 +1,5 @@
 let populacao = 2886698;
 console.log(`A cidade de Salvador tem a seguinte população: ${populacao}`)
+/// Vamos supor que nasceram mais 500 pessoinhas
+populacao += 500
+console.log(`A cidade de Salvador tem a seguinte população: ${populacao}`)
