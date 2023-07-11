030197ec04e72a6960151f4899660556018b308d
Gabriel Nunes Barbosa Nogueira <nunesnbnxn@gmail.com>
2023-07-11 15:20:12 -0300

    Atualizando repositorio

--- Módulo I/variaveis-tipos.js
+++ Módulo I/variaveis-tipos.js
@@ -4,8 +4,8 @@ let peso = 98.7 //numer
 
 let nota;
 
-console.log(nota) //undefined
+console.log(nota); //undefined
 
 nota = 20;
-
-console.log(nota)
+nota = null; //null
+console.log(nota); //number
