6bb22e96c07a424bf9c9f5f421cafb5e4e443a27
Gabriel Nunes Barbosa Nogueira <72049404+Gabriel-boop-deep@users.noreply.github.com>
2023-07-15 23:54:34 +0000

    Atualizando arquivo arrays_inicio

--- Módulo I/arrays_inicio.js
+++ Módulo I/arrays_inicio.js
@@ -5,3 +5,16 @@ let nomes = ['Chico', 'Maria', 'Rogrigo', "Gabriel"];
 //imprimindo a lista na tela
 
 console.log(nomes);
+
+//Podemos percorrer essa lista de nomes
+
+for (let nome of nomes){
+    console.log(nome);
+}
+
+//Posso verificar as iniciais de cada nome e finais de cada nome e retornar a quanridade 
+//de um caracteres que cada palavra dentro dessa lista tem
+
+for (let nome of nomes){
+    console.log(`letra inicial ${nome[0]},letra final: ${nome[nome.length-1]}, e o tamanho ${nome.length}`);
+}
