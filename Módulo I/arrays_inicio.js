248986870288472a36d6ea7c224960057ee06e6d
Gabriel Nunes Barbosa Nogueira <72049404+Gabriel-boop-deep@users.noreply.github.com>
2023-07-16 00:03:37 +0000

    Atualizando arquivo

--- Módulo I/arrays_inicio.js
+++ Módulo I/arrays_inicio.js
@@ -14,7 +14,14 @@ for (let nome of nomes){
 
 //Posso verificar as iniciais de cada nome e finais de cada nome e retornar a quanridade 
 //de um caracteres que cada palavra dentro dessa lista tem
-
+let contador =0;
+let caractere='a';
 for (let nome of nomes){
     console.log(`letra inicial ${nome[0]},letra final: ${nome[nome.length-1]}, e o tamanho ${nome.length}`);
+    for (let i =0; i<nome.length;i++){
+        if(nome[i]==caractere){
+            contador++;
+        }
+    }
 }
+console.log(`Quantidade do caractere ${caractere} na lista é de: ${contador}`);
