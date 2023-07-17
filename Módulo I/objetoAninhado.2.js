83c4f23bb7be1d829efaa1521bd1660711fdce84
Gabriel Nunes Barbosa Nogueira <nunesnbnxn@gmail.com>
2023-07-17 18:18:45 -0300

    Melhorando codigo de objeto aninhado

---
+++ Módulo I/objetoAninhado.2.js
@@ -0,0 +1,18 @@
+let carro = {
+    marca: 'Toyota',
+    modelo: 'Hailux',
+    tracao: '4x4',
+    flex: true
+}
+
+let pessoa = {
+    nome: 'Gabriel',
+    sobreNome: 'Nunes',
+    pais: 'Hildebrando',
+    carro: carro
+}
+
+console.log(pessoa.carro.modelo);
+
+
+
