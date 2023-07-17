96eccac63cd999be6d49ddc5a0c3957ae455683a
Gabriel Nunes Barbosa Nogueira <nunesnbnxn@gmail.com>
2023-07-17 18:42:05 -0300

    Enviando codigo de desestruturacao

---
+++ Módulo I/desestruturacao.js
@@ -0,0 +1,11 @@
+let pessoa = {
+    nome: 'Gabriel',
+    sobreNome: 'Nunes',
+    pais: 'Hildebrando'
+}
+// Com esse codigo se faz a desestruturacao em js
+const { nome, sobreNome, pais } = pessoa;
+
+console.log(nome, sobreNome, pais);
+
+
