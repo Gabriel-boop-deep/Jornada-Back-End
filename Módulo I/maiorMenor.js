6ae5d671e0fbf0747ab9ebcf1f923db9fa10a2bf
Gabriel Nunes Barbosa Nogueira <72049404+Gabriel-boop-deep@users.noreply.github.com>
2023-07-17 18:43:32 +0000

    Melhorando codigo

--- Módulo I/maiorMenor.js
+++ Módulo I/maiorMenor.js
@@ -13,25 +13,30 @@ let numeros = [5,4,3,7,1,9,11,15,0];
 // 4[1] > 3 [2] ? True
 // 4[1] > 7 [3] ? False
 // 3[2] > 7 [3] ? False
+function maiorMenor(numeros){
+    let j = 0,i = 0,cont_true = 0,cont_false = 0,parametro = numeros.length-1,maior,menor;
 
-let j = 0,i = 0,cont_true = 0,cont_false = 0,parametro = numeros.length-1,maior,menor;
-
-while (i<parametro){
-    //console.log(numeros[j],numeros[i])
-    i++;
-    if(i>j){
-        //console.log(numeros[j],numeros[i],numeros[j]>numeros[i]);
-        if(numeros[j]>numeros[i]==false){
-            cont_false++;
-            maior=numeros[i];
-        } else if(numeros[j]>numeros[i]==true){
-            cont_true++; 
-            menor=numeros[i]           
-        }
-        if(i==parametro){
-            i=0;
-            j++;
-        }
-    }     
+    while (i<parametro){
+        //console.log(numeros[j],numeros[i])
+        i++;
+        if(i>j){
+            //console.log(numeros[j],numeros[i],numeros[j]>numeros[i]);
+            if(numeros[j]>numeros[i]==false){
+                cont_false++;
+                maior=numeros[i];
+            } else if(numeros[j]>numeros[i]==true){
+                cont_true++; 
+                menor=numeros[i]           
+            }
+            if(i==parametro){
+                i=0;
+                j++;
+            }
+        }     
+    }
+    return [maior,menor];
 }
-console.log(maior,menor)
+//retorna o maior numero do array
+console.log(maiorMenor(numeros)[0]);   
+ //retorna o menor numero do array
+console.log(maiorMenor(numeros)[1])
